60eb6af633ede6ecc8ff7ef563ccc892fa3e7aec
HARSHIL2836R <harshilsolanki1912@gmail.com>
2024-03-18 16:58:23 +0530

    imploy attack and remove unneccesaary code

--- scriptblue.py
+++ scriptblue.py
@@ -41,33 +41,35 @@ def checkfriends(pirate , quad ):
     se = pirate.investigate_se()[1]
     sw = pirate.investigate_sw()[1]
     
+    check_tuple = ('friend', 'both')
+
     if(quad=='ne'):
-        if(up == 'friend'):
+        if(up in check_tuple):
             sum +=1 
-        if(ne== 'friend'):
+        if(ne in check_tuple):
             sum +=1 
-        if(right == 'friend'):
+        if(right in check_tuple):
             sum +=1 
     if(quad=='se'):
-        if(down == 'friend'):
+        if(down in check_tuple):
             sum +=1 
-        if(right== 'friend'):
+        if(right in check_tuple):
             sum +=1 
-        if(se == 'friend'):
+        if(se in check_tuple):
             sum +=1 
     if(quad=='sw'):
-        if(down == 'friend'):
+        if(down in check_tuple):
             sum +=1 
-        if(sw== 'friend'): 
+        if(sw in check_tuple): 
             sum +=1 
-        if(left == 'friend'):
+        if(left in check_tuple):
             sum +=1 
     if(quad=='nw'):
-        if(up == 'friend'):
+        if(up in check_tuple):
             sum +=1 
-        if(nw == 'friend'):
+        if(nw in check_tuple):
             sum +=1 
-        if(left == 'friend'):
+        if(left in check_tuple):
             sum +=1 
 
     return sum
@@ -99,64 +101,33 @@ def spread(pirate):
         return moveTo(x-1 , y-1 , pirate)
 #
 
+#my functions
+def attack(pirate):
+    up = pirate.investigate_up()[1]
+    down = pirate.investigate_down()[1]
+    left = pirate.investigate_left()[1]
+    right = pirate.investigate_right()[1]
+   
+    if up == 'enemy':
+        return 1
+    if down == 'enemy':
+        return 3
+    if left == 'enemy':
+        return 4
+    if right == 'enemy':
+        return 2
+    else:
+        return random.randint(1,4)
+    
 #Functions from samples
 def Sample1_ActPirate(pirate):
-    up = pirate.investigate_up()
-    down = pirate.investigate_down()
-    left = pirate.investigate_left()
-    right = pirate.investigate_right()
-    x, y = pirate.getPosition()
-    pirate.setSignal("")
-    s = pirate.trackPlayers()
     
-    if (
-        (up == "island1" and s[0] != "myCaptured")
-        or (up == "island2" and s[1] != "myCaptured")
-        or (up == "island3" and s[2] != "myCaptured")
-    ):
-        s = up[-1] + str(x) + "," + str(y - 1)
-        pirate.setTeamSignal(s)
-
-    if (
-        (down == "island1" and s[0] != "myCaptured")
-        or (down == "island2" and s[1] != "myCaptured")
-        or (down == "island3" and s[2] != "myCaptured")
-    ):
-        s = down[-1] + str(x) + "," + str(y + 1)
-        pirate.setTeamSignal(s)
-
-    if (
-        (left == "island1" and s[0] != "myCaptured")
-        or (left == "island2" and s[1] != "myCaptured")
-        or (left == "island3" and s[2] != "myCaptured")
-    ):
-        s = left[-1] + str(x - 1) + "," + str(y)
-        pirate.setTeamSignal(s)
-
-    if (
-        (right == "island1" and s[0] != "myCaptured")
-        or (right == "island2" and s[1] != "myCaptured")
-        or (right == "island3" and s[2] != "myCaptured")
-    ):
-        s = right[-1] + str(x + 1) + "," + str(y)
-        pirate.setTeamSignal(s)
-
-    
-    if pirate.getTeamSignal() != "":
-        s = pirate.getTeamSignal()
-        l = s.split(",")
-        x = int(l[0][1:])
-        y = int(l[1])
-    
-        return moveTo(x, y, pirate)
-
+    global frame
+    if frame < 200:
+        #to be optimised according to deploy point
+        return numpy.random.choice(numpy.arange(1, 5), p=[0.4,0.1,0.1,0.4])
     else:
-        global frame
-        if frame < 200:
-            #to be optimised according to deploy point
-            return numpy.random.choice(numpy.arange(1, 5), p=[0.4,0.1,0.1,0.4])
-        else:
-            return random.randint(1,4)
+        return attack(pirate)
 
 def Sample4_ActPirate(pirate):
     up = pirate.investigate_up()[0]
@@ -214,14 +185,7 @@ def Sample4_ActPirate(pirate):
 def Sample1_ActTeam(team):
     global phase
     phase = 1
-    l = team.trackPlayers()
-    s = team.getTeamSignal()
-
-    if s:
-        island_no = int(s[0])
-        signal = l[island_no - 1]
-        if signal == "myCaptured":
-            team.setTeamSignal("")
+    
 
 def Sample4_ActTeam(team):
     global phase
